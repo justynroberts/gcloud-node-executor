d1bf6180aba1721802c8048344e344fd164da258
Justyn Roberts <justynroberts@gmail.com>
2021-11-17 16:26:10 +0000

    Add files via upload

---
+++ src/contents/gcloud_file_copier.sh
@@ -0,0 +1,62 @@
+#!/bin/sh
+#----------------------------------------------------------------------------------------------------#
+# MIT License
+
+# Copyright (c) 2021 <Authors Here>
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+#f urnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+#----------------------------------------------------------------------------------------------------#
+GCLOUD="$RD_CONFIG_GCP_GCLOUD" 
+export PATH=$GCLOUD:$PATH
+TMPDIR=$RD_CONFIG_GCP_TMPDIR
+SESSION="$RD_CONFIG_GCP_TMPDIR""$RD_JOB_ID"
+JSON_TOKEN=$RD_CONFIG_GCP_SERVICE_ACCOUNT_TOKEN
+CMDOPTS=" "$RD_CONFIG_GCP_ADDITIONALCLI" "
+
+if [ "$RD_CONFIG_GCP_IAPTUNNELLING" == "true" ];then CMDOPTS+=" --tunnel-through-iap";fi
+echo "Key Exchange"
+echo "$JSON_TOKEN" > "$SESSION".tok 2>&1
+
+gcloud auth activate-service-account --key-file "$SESSION"'.tok' > /dev/null 2>&1
+export service_account=$(gcloud config list account --format "value(core.account)")
+export uniqueId=$(gcloud iam service-accounts describe "$service_account" --format='value(uniqueId)')
+
+ssh-keygen -t rsa -f "$SESSION"'.key' -C "USER" -N '' > /dev/null 2>&1
+chmod 400 "$SESSION"'.key'
+chmod 400 "$SESSION"'.key.pub'
+
+gcloud compute os-login ssh-keys add --key-file="$SESSION"'.key.pub' --ttl=2h > /dev/null 2>&1
+gcloud compute scp sa_"$uniqueId"@"$RD_NODE_HOSTNAME" --ssh-key-file="$SESSION".key  --project="$RD_CONFIG_GCP_PROJECT" $CMDOPTS --zone="$RD_CONFIG_GCP_ZONE" "$RD_FILE_COPY_FILE" sa_"$uniqueId"@"$RD_NODE_HOSTNAME":"$RD_FILE_COPY_DESTINATION"
+gcloud compute os-login ssh-keys remove --key-file="$SESSION"'.key.pub' > /dev/null 2>&1
+
+rm -f "$SESSION"'.key.pub'
+rm -f "$SESSION"'.key'
+rm -f "$SESSION"'.tok'
+
+
+
+
+
+
+
+
+
+
+
+
